d19b001d674d66a3965a3418a155462dbe4fa80e
phuangpheth PHOUMINITH <pao.phouminith@gmail.com>
2024-10-06 10:57:55 +0700

    To use express json and urlencoded

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const port =  process.env.PORT || 8089;
 
 app.get("/", (req, res) => {
