d4dc6123d6d00fccbe5033552816ce6962062173
phuangpheth PHOUMINITH <pao.phouminith@gmail.com>
2024-10-06 11:09:59 +0700

    To use morgan for logging

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 import express from "express";
 import httpStatus from "http-status-codes";
 import dotenv from "dotenv";
+import morgan from "morgan";
 
 dotenv.config();
 
@@ -9,17 +10,22 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
 
 const port =  process.env.PORT || 8089;
 
 app.get("/", (req, res) => {
   res.status(httpStatus.OK).json({
-    message: "Hello World",
+    code: httpStatus.OK,
+    status: "SUCCESS",
+    message: "Hello World...",
   });
 });
 
 app.get("/greeting/:name", (req, res) => {
   res.status(httpStatus.OK).json({
+    code: httpStatus.OK,
+    status: "SUCCESS",
     message: `Hello ${req.params.name}`,
   });
 })
