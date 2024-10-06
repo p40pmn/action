6f4bd79d7f95169ed0e896cc292db6b39afa1db4
phuangpheth PHOUMINITH <pao.phouminith@gmail.com>
2024-10-06 10:55:29 +0700

    To init simple http server

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+// Init the http server from  express
+import express from "express";
+import httpStatus from "http-status-codes";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const port =  process.env.PORT || 8089;
+
+app.get("/", (req, res) => {
+  res.status(httpStatus.OK).json({
+    message: "Hello World",
+  });
+});
+
+app.get("/greeting/:name", (req, res) => {
+  res.status(httpStatus.OK).json({
+    message: `Hello ${req.params.name}`,
+  });
+})
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+})
